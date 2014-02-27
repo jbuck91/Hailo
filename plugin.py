c911b1f7d8897a2b3aa898e2d7b0c7a015802670
Joe Buck <xgenoblast@gmail.com>
2014-02-27 04:31:50 +0000

    Fix some whitespace issues

--- plugin.py
+++ plugin.py
@@ -5,19 +5,19 @@
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
-#   * Redistributions of source code must retain the above copyright notice,
-#     this list of conditions, and the following disclaimer.
-#   * Redistributions in binary form must reproduce the above copyright notice,
-#     this list of conditions, and the following disclaimer in the
-#     documentation and/or other materials provided with the distribution.
-#   * Neither the name of the author of this software nor the name of
-#     contributors to this software may be used to endorse or promote products
-#     derived from this software without specific prior written consent.
+#	 * Redistributions of source code must retain the above copyright notice,
+#		 this list of conditions, and the following disclaimer.
+#	 * Redistributions in binary form must reproduce the above copyright notice,
+#		 this list of conditions, and the following disclaimer in the
+#		 documentation and/or other materials provided with the distribution.
+#	 * Neither the name of the author of this software nor the name of
+#		 contributors to this software may be used to endorse or promote products
+#		 derived from this software without specific prior written consent.
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# ARE DISCLAIMED.	IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
 # LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
 # CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
 # SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
@@ -77,14 +77,13 @@ class Hailo(callbacks.Plugin):
 			msg = None
 		return msg
 
-        def brainstats (self,irc,msg,args,channel):
+	def brainstats (self,irc,msg,args,channel):
 		"""[<channel>]
-
 		return brain stats"""
-                m = self.callHailo(channel,'-s','')
-                if m != None:
-                	irc.reply(m)
-	brainstats = wrap(brainstats,['op'])
+		m = self.callHailo(channel,'-s','')
+		if m != None:
+			irc.reply(m)
+		brainstats = wrap(brainstats,['op'])
 
 	def doPrivmsg (self,irc,msg):
 		(targets, t) = msg.args
